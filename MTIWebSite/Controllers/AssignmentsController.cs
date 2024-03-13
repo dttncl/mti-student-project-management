657d0493417826d8cca80b59c9cafc61d6334046
dttncl <82695034+dttncl@users.noreply.github.com>
2024-03-13 12:03:16 -0400

    added controllers

---
+++ MTIWebSite/Controllers/AssignmentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MTIWebSite.Models;
+
+namespace MTIWebSite.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        private MTIDatabaseEntities db = new MTIDatabaseEntities();
+
+        // GET: Assignments
+        public ActionResult Index()
+        {
+            var assignments = db.Assignments.Include(a => a.Projects).Include(a => a.Students);
+            return View(assignments.ToList());
+        }
+
+        // GET: Assignments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignments assignments = db.Assignments.Find(id);
+            if (assignments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignments);
+        }
+
+        // GET: Assignments/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProjectCode = new SelectList(db.Projects, "ProjectCode", "ProjectTitle");
+            ViewBag.StudentNumber = new SelectList(db.Students, "StudentNumber", "FirstName");
+            return View();
+        }
+
+        // POST: Assignments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AssignmentId,ProjectCode,StudentNumber,AssignmentDate")] Assignments assignments)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assignments.Add(assignments);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProjectCode = new SelectList(db.Projects, "ProjectCode", "ProjectTitle", assignments.ProjectCode);
+            ViewBag.StudentNumber = new SelectList(db.Students, "StudentNumber", "FirstName", assignments.StudentNumber);
+            return View(assignments);
+        }
+
+        // GET: Assignments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignments assignments = db.Assignments.Find(id);
+            if (assignments == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectCode = new SelectList(db.Projects, "ProjectCode", "ProjectTitle", assignments.ProjectCode);
+            ViewBag.StudentNumber = new SelectList(db.Students, "StudentNumber", "FirstName", assignments.StudentNumber);
+            return View(assignments);
+        }
+
+        // POST: Assignments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AssignmentId,ProjectCode,StudentNumber,AssignmentDate")] Assignments assignments)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(assignments).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectCode = new SelectList(db.Projects, "ProjectCode", "ProjectTitle", assignments.ProjectCode);
+            ViewBag.StudentNumber = new SelectList(db.Students, "StudentNumber", "FirstName", assignments.StudentNumber);
+            return View(assignments);
+        }
+
+        // GET: Assignments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignments assignments = db.Assignments.Find(id);
+            if (assignments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignments);
+        }
+
+        // POST: Assignments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Assignments assignments = db.Assignments.Find(id);
+            db.Assignments.Remove(assignments);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MTIWebSite/Controllers/ProjectsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MTIWebSite.Models;
+
+namespace MTIWebSite.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private MTIDatabaseEntities db = new MTIDatabaseEntities();
+
+        // GET: Projects
+        public ActionResult Index()
+        {
+            return View(db.Projects.ToList());
+        }
+
+        // GET: Projects/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Projects projects = db.Projects.Find(id);
+            if (projects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projects);
+        }
+
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Projects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProjectCode,ProjectTitle,DueDate")] Projects projects)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(projects);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(projects);
+        }
+
+        // GET: Projects/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Projects projects = db.Projects.Find(id);
+            if (projects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projects);
+        }
+
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProjectCode,ProjectTitle,DueDate")] Projects projects)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(projects).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(projects);
+        }
+
+        // GET: Projects/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Projects projects = db.Projects.Find(id);
+            if (projects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projects);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Projects projects = db.Projects.Find(id);
+            db.Projects.Remove(projects);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
